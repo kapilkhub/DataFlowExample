159155b1f4c1e358a319329a5a2d9ee0fad2bd4d
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2023-06-11 22:37:55 +0400

    commit for producer consumer

---
+++ ProducerConsumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+async Task<int> ConsumeAsync(IReceivableSourceBlock<byte[]> sourceBlock)
+{
+	int bytesProcessed = 0;
+
+	while (await sourceBlock.OutputAvailableAsync()) 
+	{
+		while (sourceBlock.TryReceive(out byte[] data))
+		{
+			bytesProcessed += data.Length;
+		}
+	}
+
+	return bytesProcessed;
+}
+
+
+void  Produce(ITargetBlock<byte[]> targetBlock)
+{
+	Random rand = new Random();
+	for (int i = 0; i < 100; ++i)
+	{
+		var buffer = new byte[1024];
+		rand.NextBytes(buffer);
+		targetBlock.Post(buffer);
+	}
+
+	targetBlock.Complete();
+}
+
+
+var buffer = new BufferBlock<byte[]>();
+
+var consumertask =ConsumeAsync(buffer);
+Produce(buffer);
+
+
+Console.WriteLine($"Data processed {await consumertask}");
+
